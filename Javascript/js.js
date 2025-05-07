f2a52a0bd7b60aee9f7034bddce9f28c019cbc84
Gabbsos <gabbysos230@gmail.com>
2025-05-07 13:56:01 +1000

    GRIFFITH!!!!!!!!!

---
+++ Javascript/js.js
@@ -0,0 +1,16 @@
+
+
+function appear()
+{
+document.getElementById("thediv").style.display = "block"
+}
+
+function disappear()
+{
+document.getElementById("thediv").style.display = "none";
+}
+
+
+var day = document.getElementById("day").value;
+var month = document.getElementById("month").value;
+var year = document.getElementById("year").value;
