5da8f676098d176492a77e7359be9e446cda6bda
Gabbsos <gabbysos230@gmail.com>
2025-05-08 11:49:45 +1000

    not done yet

--- Javascript/js.js
+++ Javascript/js.js
@@ -2,15 +2,32 @@
 
 function appear()
 {
-document.getElementById("thediv").style.display = "block"
+    document.getElementById("thediv").style.display = "block"
 }
 
 function disappear()
 {
-document.getElementById("thediv").style.display = "none";
+    document.getElementById("thediv").style.display = "none";
 }
 
 
-var day = document.getElementById("day").value;
-var month = document.getElementById("month").value;
-var year = document.getElementById("year").value;
+
+function colour()
+{
+    let today = new Date();
+    const map1 = new Map();
+    map1.set("day",31)
+    map1.set("month",12)
+    map1.set("year", today.getFullYear())
+    
+    for (const [key, value] of map1){
+        (key > value || key < 0) ? document.getElementById(key).style.backgroundColor = "red" : key / value *255
+    }
+    
+    var day = document.getElementById("day").value;
+    var month = document.getElementById("month").value / 12 * 255;
+    var year = document.getElementById("year").value % 255;
+
+
+    document.getElementById("thediv").style.backgroundColor = `rgb(${year},${day},${month})`;
+}
