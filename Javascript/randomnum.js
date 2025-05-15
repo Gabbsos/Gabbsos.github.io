ffebc4f93fe2a82506a7b12d12bd30f961839bd0
Gabbsos <gabbysos230@gmail.com>
2025-05-15 20:54:15 +1000

    nearly

--- Javascript/randomnum.js
+++ Javascript/randomnum.js
@@ -1,55 +1,86 @@
-
-
 let it = 0;
 let min = 0;
 let max = 0;
 
-function makenum(){
-    
-    while(true){
-
-        while (true){
+function makenum() {
+    while (true) {
+        
+        while (true) {
             min = prompt("Please type in a minimum value");
-            if(!Number.isInteger(parseInt(min))){
+            if (min === null) {
+                document.getElementById("text").textContent = "Process canceled!";
+                return;  
+            }
+            min = parseInt(min);
+            if (!Number.isInteger(min)) {
+                document.getElementById("text").textContent = "Type an integer";
                 continue;
-            }else{
-                break;
+            } else {
+                break;  
             }
         }
 
-        while(true){
+        
+        while (true) {
             max = prompt("Please type in a maximum value");
-            if(!Number.isInteger(parseInt(max))){
+            if (max === null) {
+                document.getElementById("text").textContent = "Process canceled!";
+                return;  
+            }
+            max = parseInt(max);
+            if (!Number.isInteger(max)) {
+                document.getElementById("text").textContent = "Type an integer";
                 continue;
-            }else{
-                break;
+            } else {
+                break;  
             }
         }
-        if(min>max){
-            continue;
-        }else{
-            break;
+
+        
+        if (min > max) {
+            document.getElementById("text").textContent = "Make sure max > min";
+            continue; 
+        } else {
+            break; 
         }
     }
 
-    it = randomnum(min,max);
+    it = randomnum(min, max);
+    console.log(it)
 }
 
-function randomnum(min,max){
-    return Math.floor(Math.random()* (max - min + 1) + min);
+function randomnum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function guess(min,max){
-    
-    while(true){
-    let num = prompt(`Guess a number between ${min} and ${max}`)
-    if
-    
+function guess() {
+    while (true) {
+        let num = prompt(`Guess a number between ${min} and ${max}`);
+        if (num === null) {
+            document.getElementById("text").textContent = "You canceled the guessing game.";
+            return; 
+        }
+
+        num = parseInt(num);
+        if (!Number.isInteger(num)) {
+            document.getElementById("text").textContent = "Type an integer";
+            continue;
+        } else if (num < min || num > max) {
+            document.getElementById("text").textContent = "Make sure you guess within min and max";
+            continue;
+        } else {
+            if (num === it) {
+                document.getElementById("text").textContent = "You got it right :)";
+                break; 
+            } else if (num < it) {
+                document.getElementById("text").textContent = "Higher";
+                continue;
+            } else if (num > it) {
+                document.getElementById("text").textContent = "Lower";
+                continue;
+            }
+        }
     }
 }
-
-
-
-
     
 
