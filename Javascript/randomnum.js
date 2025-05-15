69502df15dced4dbc4d9921a89a78cd25f1d5110
Gabbsos <gabbysos230@gmail.com>
2025-05-15 11:48:36 +1000

    new

--- Javascript/randomnum.js
+++ Javascript/randomnum.js
@@ -1,20 +1,55 @@
 
-function hol(){
 
-    var it = Math.floor(Math.random()*101);
-    var num = parseInt(document.getElementById("num").value)
+let it = 0;
+let min = 0;
+let max = 0;
 
-    if (!Number.isInteger(num)){
-        document.getElementById("la").textContent = "Make sure the value is an int";
-    }else{
-        if(num > it){
-            document.getElementById("la").textContent = "Lower"
-        }else if (num == it){
-            document.getElementById("la").textContent = "You got it right"
+function makenum(){
+    
+    while(true){
+
+        while (true){
+            min = prompt("Please type in a minimum value");
+            if(!Number.isInteger(parseInt(min))){
+                continue;
+            }else{
+                break;
+            }
+        }
+
+        while(true){
+            max = prompt("Please type in a maximum value");
+            if(!Number.isInteger(parseInt(max))){
+                continue;
+            }else{
+                break;
+            }
+        }
+        if(min>max){
+            continue;
         }else{
-            document.getElementById("la").textContent = "Higher"
+            break;
         }
     }
+
+    it = randomnum(min,max);
+}
+
+function randomnum(min,max){
+    return Math.floor(Math.random()* (max - min + 1) + min);
+}
+
+function guess(min,max){
+    
+    while(true){
+    let num = prompt(`Guess a number between ${min} and ${max}`)
+    if
+    
+    }
+}
+
+
+
+
     
 
-}
