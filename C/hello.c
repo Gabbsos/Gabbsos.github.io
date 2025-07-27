a1579c4858d9d9778d693dee3789671e031a93ac
Gabbsos <gabbysos230@gmail.com>
2025-07-27 22:02:58 +1000

    new

--- C/hello.c
+++ C/hello.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
 
 int main(){
-    int myNum = 15;
-    printf("Hello World\n");
-    printf("%d", myNum);
+
+    int myNums[] = {25,50,75,100};
+    int length = sizeof(myNums) / sizeof(myNums[0]);
+    
+    for (int i = 0; i < length; i++){
+        printf("%d\n", myNums[i]);
+    }
+
+
+
+    int nums[2][3] = {
+        {1,4,2},
+        {3,6,8}
+    };
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+            printf("%d",nums[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    
+    char greet[] = "Hello World";
+    printf("%s", greet);
+
+
     return 0;
 }
